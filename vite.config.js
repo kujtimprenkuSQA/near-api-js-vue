621155c0e4f5b00540df2e0c5a717ba70532f595
Kujtim Prenku <kujtim.prenku@sqa-consulting.com>
2023-03-02 20:44:59 +0100

    Install gh-pages and deploy app.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "near-api-js-vue",
   plugins: [vue()],
   build: {
     commonjsOptions: { include: [] },
