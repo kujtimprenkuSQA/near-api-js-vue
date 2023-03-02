67a0c789fda609c400b8b3d4a6ec8334bf69f402
Kujtim Prenku <kujtim.prenku@sqa-consulting.com>
2023-03-02 23:33:25 +0100

    Added alternative way to add polyfills in Vite/Vue Add polyfills for buffer and process via @esbuild-plugins/node-globals-polyfill and @esbuild-plugins/node-modules-polyfill

--- src/shims.js
+++
@@ -1,7 +0,0 @@
-import * as process from "process";
-import { Buffer } from "buffer";
-Object.assign(self, {
-  process,
-  global: self,
-  Buffer,
-});
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,9 @@
+// npm install --dev @esbuild-plugins/node-globals-polyfill
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+// npm install --dev @esbuild-plugins/node-modules-polyfill
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
+// You don't need to add this to deps, it's included by @esbuild-plugins/node-modules-polyfill
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -6,9 +12,28 @@ export default defineConfig({
   base: "near-api-js-vue",
   plugins: [vue()],
   build: {
-    commonjsOptions: { include: [] },
+    commonjsOptions: {
+      include: []
+    },
+    rollupOptions: {
+      plugins: [
+        // Enable rollup polyfills plugin
+        // used during production bundling
+        rollupNodePolyFill()
+      ]
+    }
   },
   optimizeDeps: {
     disabled: false,
+    esbuildOptions: {
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true
+        }),
+        NodeModulesPolyfillPlugin()
+      ]
+    }
   },
 })
